057c1d1445e7ded8b46055f7d9a744193089d422
Md. Harish-Uz-Jaman Mridha Raju <rajubd49@gmail.com>
2018-03-28 13:16:26 +0600

    Initial Commit

---
+++ AssetManagement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AssetManagement
+//
+//  Created by BS-195 on 3/28/18.
+//  Copyright © 2018 rajubd49. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
